2394521e01690c0ddd857f662148d4c892434cad
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-25 00:40:14 +0400

    new vers 2.8

--- main.py
+++ main.py
@@ -306,6 +306,7 @@ async def all_functions(message: types.Message):
             user_first_id = match.group(1)
         else:
             user_first_id = None
+        await bot.send_message(user_first_id, text_to_search)
         await bot.send_message(user_first_id, user_first_id)
         lang = db.select_language(user_first_id)
         if photo_to_search is None:
