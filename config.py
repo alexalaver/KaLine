70af817b2e3fe7a88aa88f0fb79a761391cb33e8
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-27 17:18:41 +0400

    nedorobotki

--- config.py
+++ config.py
@@ -136,11 +136,11 @@ def BUY_eSIM_TEXT(lang):
 
 def CONFIRM_ORDERS_USER_TEXT(lang, order_id):
     if lang == "arm":
-        return f"Հարգելի օգտատեր Ձեր վճարումը հաստատված է: Ձեր Order ID-ն է \\`\n\n`{order_id}`"
+        return f"Հարգելի օգտատեր Ձեր վճարումը հաստատված է: Ձեր Order ID-ն է \\`\n\n`{order_id}`\n\nՄուտքագրեք այն 'Ակտիվացնել eSIM' դաշտում QR կոդ ստանալու համար:"
     elif lang == "rus":
-        return f"Уважаемый пользователь, ваш платеж подтвержден. Ваш Order ID:\n\n`{order_id}`"
+        return f"Уважаемый пользователь, ваш платеж подтвержден. Ваш Order ID:\n\n`{order_id}`\n\nВведите его в поле «Активировать eSIM», чтобы получить QR-код."
     elif lang == "eng":
-        return f"Dear user, your payment has been confirmed. Your Order ID is:\n\n`{order_id}`"
+        return f"Dear user, your payment has been confirmed. Your Order ID is:\n\n`{order_id}`\n\nEnter it in the 'Activate eSIM' field to receive a QR code."
 
 def BACK_TEXT(lang):
     if lang == "arm":
